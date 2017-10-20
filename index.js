5fbc55a3de577326ca2f5e4142e00b77b045b746
Tsuyoshi Akiyama <ka090824@outlook.com>
2017-10-20 04:19:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(array, name) {
   array.push(name)
-  return `Welcome, ${name}. You are number ${array.indexOf(name)} in line.`
+  return `Welcome, ${name}. You are number ${array.indexOf(name) + 1} in line.`
 }
 
 function nowServing(array) {
